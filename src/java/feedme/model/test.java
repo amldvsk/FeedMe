680df00adbb8e2d2081779f9e466879fe243b013
Nadavbi <nadavbi87@gmail.com>
2015-05-03 16:26:24 +0300

    Merge origin/master
    Conflicts:
    	src/java/feedme/model/DBRestaurantsManagement.java

--- src/java/feedme/model/test.java
+++ src/java/feedme/model/test.java
@@ -20,7 +20,7 @@ import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.eclipse.jdt.internal.compiler.ast.ASTNode;
+
 
 /**
  *
